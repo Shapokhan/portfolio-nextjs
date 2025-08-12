1f62e4588448ff72836672655564a66db3bcd84e
kashifanwar313 <kashifanwar2015@gmail.com>
2025-08-12 13:02:16 +0500

    Basic Admin Dashboard.

--- components/ReusableComponent/NavBar/Navbar.tsx
+++ components/ReusableComponent/NavBar/Navbar.tsx
@@ -1,7 +1,10 @@
-import { LogOut, Moon, Settings, User } from 'lucide-react';
+'use client';
+
+import { LogOut, Moon, Settings, Sun, User } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
 
 import {
   DropdownMenu,
@@ -11,25 +14,48 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { useTheme } from 'next-themes';
+import { SidebarTrigger } from '@/components/ui/sidebar';
 
 export default function Navbar(){
+  const { theme, setTheme } = useTheme();
   return (
-    <nav className="p-4 flex items-center justify-between">
+    <nav className="p-4 flex items-center justify-between sticky top-0 bg-background z-10">
       {/* Left */}
-      collapseButton
+      <SidebarTrigger/>
       {/* Right */}
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
-        <Moon/>
-         
+        {/* THEME MENU */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        {/* USER MENU */}
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Avatar>
               <AvatarImage src="https://avatars.githubusercontent.com/u/29621916" />
-              <AvatarFallback>CN</AvatarFallback>
+              <AvatarFallback>KA</AvatarFallback>
             </Avatar>
           </DropdownMenuTrigger>
-          <DropdownMenuContent>
+          <DropdownMenuContent sideOffset={10}>
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
@@ -37,11 +63,11 @@ export default function Navbar(){
               Profile
             </DropdownMenuItem>
             <DropdownMenuItem>
-              <Settings/>
+              <Settings className="h-[1.2rem] w-[1.2rem] mr-2"/>
               Settings
             </DropdownMenuItem>
-            <DropdownMenuItem>
-              <LogOut/>
+            <DropdownMenuItem variant="destructive">
+              <LogOut className="h-[1.2rem] w-[1.2rem] mr-2"/>
               Logout
             </DropdownMenuItem>
           </DropdownMenuContent>
--- components/ReusableComponent/SideBar/AppSidebar.tsx
+++ components/ReusableComponent/SideBar/AppSidebar.tsx
@@ -1,7 +1,109 @@
-import React from 'react'
+import {
+  Home as HomeIcon,
+  Plus as PlusIcon,
+  Clock as ClockIcon,
+  Settings as SettingsIcon,
+  FileStack,
+  User2 as UserIcon,
+  ChevronUp,
+  Projector,
+  ChevronDown,
+} from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 
-export default function AppSidebar(){
+const menuItems = [
+  { title: "Dashboard", url: "/", icon: HomeIcon },
+  { title: "New Order", url: "/new-order", icon: PlusIcon },
+  { title: "Order History", url: "/orders", icon: ClockIcon },
+  { title: "Generate Documents", url: "/documents", icon: FileStack },
+  { title: "Settings", url: "/settings", icon: SettingsIcon },
+];
+
+export default function AppSidebar() {
   return (
-    <div>AppSidebar</div>
-  )
-}
+    <Sidebar collapsible="icon" className="overflow-hidden">
+      <SidebarHeader className="py-4">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton asChild>
+              <Link href="/">
+                <Image src="/logo.png" alt="logo" width={20} height={20} />
+                <span>Kashif Anwar</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+
+      <SidebarSeparator />
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {menuItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                  {item.title === "Inbox" && (
+                    <SidebarMenuBadge>24</SidebarMenuBadge>
+                  )}
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <UserIcon /> Kashif Anwar <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>Account</DropdownMenuItem>
+                <DropdownMenuItem>Setting</DropdownMenuItem>
+                <DropdownMenuItem>Sign out</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
